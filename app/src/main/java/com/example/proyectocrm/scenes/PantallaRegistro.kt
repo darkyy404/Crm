8ca3dd62b167094e53c03e9442d5d4ad3f308167
=Alejandro Yepes <=alejandroyg3@gmail.com>
2024-11-10 02:01:17 +0100

    Creacion de Componentes y diseño

--- app/src/main/java/com/example/proyectocrm/scenes/PantallaRegistro.kt
+++ app/src/main/java/com/example/proyectocrm/scenes/PantallaRegistro.kt
@@ -1,2 +1,203 @@
 package com.example.proyectocrm.scenes
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.proyectocrm.R
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun RegisterScreen() {
+    val name = remember { mutableStateOf(TextFieldValue("")) }
+    val lastName = remember { mutableStateOf(TextFieldValue("")) }
+    val email = remember { mutableStateOf(TextFieldValue("")) }
+    val phone = remember { mutableStateOf(TextFieldValue("")) }
+    val password = remember { mutableStateOf(TextFieldValue("")) }
+    val confirmPassword = remember { mutableStateOf(TextFieldValue("")) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFEDF1F3)), // Fondo gris claro
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    ) {
+        // Ícono de retroceso
+        Icon(
+            imageVector = Icons.Default.ArrowBack, // Icono de retroceso
+            contentDescription = "Back",
+            modifier = Modifier
+                .padding(16.dp)
+                .size(24.dp)
+                .clickable { /* Acción de volver atrás */ },
+            tint = Color.Black
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(
+            text = "Registro",
+            fontSize = 28.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color(0xFF1F1F1F)
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Text(
+            text = "¡Crea una cuenta para continuar!",
+            fontSize = 14.sp,
+            color = Color(0xFF5A5A5A),
+            textAlign = TextAlign.Center
+        )
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        // Campo de nombre
+        OutlinedTextField(
+            value = name.value,
+            onValueChange = { name.value = it },
+            label = { Text("Nombre") },
+            modifier = Modifier
+                .fillMaxWidth(0.85f)
+                .height(56.dp)
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        // Campo de apellido
+        OutlinedTextField(
+            value = lastName.value,
+            onValueChange = { lastName.value = it },
+            label = { Text("Apellidos") },
+            modifier = Modifier
+                .fillMaxWidth(0.85f)
+                .height(56.dp)
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        // Campo de email
+        OutlinedTextField(
+            value = email.value,
+            onValueChange = { email.value = it },
+            label = { Text("Email") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+            modifier = Modifier
+                .fillMaxWidth(0.85f)
+                .height(56.dp)
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        // Campo de número telefónico
+        OutlinedTextField(
+            value = phone.value,
+            onValueChange = { phone.value = it },
+            label = { Text("Número Telefónico") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Phone),
+            modifier = Modifier
+                .fillMaxWidth(0.85f)
+                .height(56.dp)
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        // Campo de contraseña
+        OutlinedTextField(
+            value = password.value,
+            onValueChange = { password.value = it },
+            label = { Text("Establecer contraseña") },
+            visualTransformation = PasswordVisualTransformation(),
+            trailingIcon = {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_visibility), // Ícono de visibilidad
+                    contentDescription = "Visibility Icon",
+                    modifier = Modifier.size(20.dp)
+
+                )
+            },
+            modifier = Modifier
+                .fillMaxWidth(0.85f)
+                .height(56.dp)
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        // Campo de confirmar contraseña
+        OutlinedTextField(
+            value = confirmPassword.value,
+            onValueChange = { confirmPassword.value = it },
+            label = { Text("Repetir contraseña") },
+            visualTransformation = PasswordVisualTransformation(),
+            trailingIcon = {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_visibility), // Ícono de visibilidad
+                    contentDescription = "Visibility Icon",
+                    modifier = Modifier.size(20.dp)
+
+                )
+            },
+            modifier = Modifier
+                .fillMaxWidth(0.85f)
+                .height(56.dp)
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = { /* Acción de registro */ },
+            modifier = Modifier
+                .fillMaxWidth(0.85f)
+                .height(50.dp),
+            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF007AFF)),
+            shape = RoundedCornerShape(8.dp)
+        ) {
+            Text(text = "Registro", color = Color.White, fontWeight = FontWeight.SemiBold)
+        }
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Row {
+            Text(
+                text = "¿Ya tienes una cuenta? ",
+                color = Color.Gray,
+                fontSize = 14.sp
+            )
+            Text(
+                text = "Iniciar sesión",
+                color = Color(0xFF007AFF),
+                fontSize = 14.sp,
+                modifier = Modifier.clickable { /* Acción de iniciar sesión */ }
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewRegisterScreen() {
+    RegisterScreen()
+}
