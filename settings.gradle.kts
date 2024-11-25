d0c6b81566016cb615bbd1540692f477d5c1b8c1
=Alejandro Yepes <=alejandroyg3@gmail.com>
2024-11-25 19:52:22 +0100

    feat: Implementacion de https://jitpack.io en settings.gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,9 +16,11 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-    }
+        maven {
+            url = uri("https://jitpack.io") // Asegúrate de usar `uri` correctamente aquí
+        }
 }
 
 rootProject.name = "ProyectoCrm"
 include(":app")
- 
+}
