8525b53b708f688ef794aa346a7aaafce60c3589
=Alejandro Yepes <=alejandroyg3@gmail.com>
2024-11-26 22:55:07 +0100

    implementacion del metodo importarDatosFirebase al eejcutar la aplicacion (Solo una vez)

--- app/src/main/java/com/example/proyectocrm/MainActivity.kt
+++ app/src/main/java/com/example/proyectocrm/MainActivity.kt
@@ -1,24 +1,32 @@
 package com.example.proyectocrm
 
-
+import com.example.proyectocrm.scripts.DataImporter // Asegúrate de que esta importación sea correcta
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 
 class MainActivity : ComponentActivity() {
     private lateinit var navHostController: NavHostController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             navHostController = rememberNavController()
             NavigationWrapper(navHostController)
         }
+
+        // Llama al método para importar datos en Firebase
+        importarDatosFirebase()
+    }
+
+    /**
+     * Función para importar datos en Firebase utilizando el DataImporter.
+     */
+    private fun importarDatosFirebase() {
+        DataImporter.importarDatosAutomatizados()
     }
 }
