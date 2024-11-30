2436e78834fa7447120b631ae5a647eecb80b1ce
=Alejandro Yepes <=alejandroyg3@gmail.com>
2024-11-30 19:53:52 +0100

    Logica de PantallaCambiarMetodoAcceso.kt

--- app/src/main/java/com/example/proyectocrm/scenes/acceso/PantallaCambiarMetodoAcceso.kt
+++ app/src/main/java/com/example/proyectocrm/scenes/acceso/PantallaCambiarMetodoAcceso.kt
@@ -1,2 +1,53 @@
 package com.example.proyectocrm.scenes.acceso
 
+import android.content.Context
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+
+@Composable
+fun PantallaCambiarMetodoAcceso(navHostController: NavHostController) {
+    val context = LocalContext.current
+    var selectedOption by remember { mutableStateOf(leerPreferencia(context, "auth_method")) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Cambiar método de acceso", fontSize = 20.sp)
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        RadioButtonWithLabel(
+            label = "Usar mi huella dactilar",
+            isSelected = selectedOption == "fingerprint",
+            onClick = { selectedOption = "fingerprint" }
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        RadioButtonWithLabel(
+            label = "Usar un PIN",
+            isSelected = selectedOption == "pin",
+            onClick = { selectedOption = "pin" }
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(onClick = {
+            guardarPreferencia(context, "auth_method", selectedOption ?: "")
+            navHostController.navigate("pantallaPrincipal")
+        }) {
+            Text("Guardar Cambios")
+        }
+    }
+}
+
